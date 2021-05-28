f3d4dccdaa168a4ec62e08bd38700dcbf999ea63
superstark02 <superstark02@gmail.com>
2021-05-29 02:13:08 +0530

    123

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import Dashboard from './Pages/Dashboard';
+import Single from './Pages/Single'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/product' component={Single} />
         </Switch>
       </Router>
     </div>
