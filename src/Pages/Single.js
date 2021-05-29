ac749bccfe87f2e33e38c4b4365b0c5862fe6e37
superstark02 <superstark02@gmail.com>
2021-05-29 20:22:12 +0530

    123

--- src/Pages/Single.js
+++ src/Pages/Single.js
@@ -9,54 +9,38 @@ export class Single extends Component {
         return (
             <div>
                 <AppBar />
-                <div class="head-bread" style={{ paddingTop: "10vh" }} >
-                    <div class="container">
-                        <ol class="breadcrumb">
+                <div className="head-bread" style={{ paddingTop: "10vh" }} >
+                    <div className="container">
+                        <ol className="breadcrumb">
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Men</a></li>
-                            <li class="active">Shop</li>
+                            <li className="active">Shop</li>
                         </ol>
                     </div>
                 </div>
 
-                <div class="showcase-grid">
-                    <div class="container">
-                        <div class="col-md-8 showcase">
-                            <div class="flexslider">
-                                <ul class="slides">
-                                    <li data-thumb="images/show.jpg">
-                                        <div class="thumb-image"> <img src="images/show.jpg" data-imagezoom="true" class="img-responsive" /> </div>
-                                    </li>
-                                    <li data-thumb="images/show1.jpg">
-                                        <div class="thumb-image"> <img src="images/show1.jpg" data-imagezoom="true" class="img-responsive" /> </div>
-                                    </li>
-                                    <li data-thumb="images/show2.jpg">
-                                        <div class="thumb-image"> <img src="images/show2.jpg" data-imagezoom="true" class="img-responsive" /> </div>
-                                    </li>
-                                    <li data-thumb="images/show3.jpg">
-                                        <div class="thumb-image"> <img src="images/show3.jpg" data-imagezoom="true" class="img-responsive" /> </div>
-                                    </li>
-                                </ul>
-                                <div class="clearfix"></div>
-                            </div>
+                <div className="showcase-grid">
+                    <div className="container">
+                        <div className="col-md-8 showcase">
+                            <MySlider/>
                         </div>
-                        <div class="col-md-4 showcase">
-                            <div class="showcase-rt-top">
-                                <div class="pull-left shoe-name">
+                        <div className="col-md-4 showcase">
+                            <div className="showcase-rt-top">
+                                <div className="pull-left shoe-name">
                                     <h3>Stone Grenade RTL</h3>
                                     <p>BLUETOOTH SPEAKERS</p>
                                     <h4>₹1,002.00</h4>
                                 </div>
-                                <div class="clearfix"></div>
+                                <div className="clearfix"></div>
                             </div>
-                            <hr class="featurette-divider" />
-                            <div class="shocase-rt-bot">
-                                <div class="float-qty-chart">
+                            <hr className="featurette-divider" />
+                            <div className="shocase-rt-bot">
+                                <div className="float-qty-chart">
                                     <ul>
                                         {/*
-                                            <li class="qty">
+                                            <li className="qty">
                                                 <h3>Size Chart</h3>
-                                                <select class="form-control siz-chrt">
+                                                <select className="form-control siz-chrt">
                                                     <option>6 US</option>
                                                     <option>7 US</option>
                                                     <option>8 US</option>
@@ -67,9 +51,9 @@ export class Single extends Component {
                                             </li>
                                         */}
 
-                                        <li class="qty">
+                                        <li className="qty">
                                             <h4>QTY</h4>
-                                            <select class="form-control qnty-chrt">
+                                            <select className="form-control qnty-chrt">
                                                 <option>1</option>
                                                 <option>2</option>
                                                 <option>3</option>
@@ -80,16 +64,16 @@ export class Single extends Component {
                                             </select>
                                         </li>
                                     </ul>
-                                    <div class="clearfix"></div>
+                                    <div className="clearfix"></div>
                                 </div>
                                 <ul>
-                                    <li class="ad-2-crt simpleCart_shelfItem">
-                                        <a class="btn item_add" href="#" role="button">Add To Cart</a>
-                                        <a class="btn" href="#" role="button">Buy Now</a>
+                                    <li className="ad-2-crt simpleCart_shelfItem">
+                                        <a className="btn item_add" href="#" role="button">Add To Cart</a>
+                                        <a className="btn" href="#" role="button">Buy Now</a>
                                     </li>
                                 </ul>
                             </div>
-                            <div class="showcase-last">
+                            <div className="showcase-last">
                                 <h3>product details</h3>
                                 <ul>
                                     <li>Internal bootie wraps your foot for a sock-like fit</li>
@@ -100,39 +84,39 @@ export class Single extends Component {
                                 </ul>
                             </div>
                         </div>
-                        <div class="clearfix"></div>
+                        <div className="clearfix"></div>
                     </div>
                 </div>
 
-                <div class="specifications">
-                    <div class="container">
+                <div className="specifications">
+                    <div className="container">
                         <h3>Item Details</h3>
-                        <div class="detai-tabs">
-                            <ul class="nav nav-pills tab-nike" role="tablist">
-                                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Highlights</a></li>
+                        <div className="detai-tabs">
+                            <ul className="nav nav-pills tab-nike" role="tablist">
+                                <li role="presentation" className="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Highlights</a></li>
                                 <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Description</a></li>
-                                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Terms & conditiona</a></li>
+                                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Terms & conditions</a></li>
                             </ul>
-                            <div class="tab-content">
-                                <div role="tabpanel" class="tab-pane active" id="home">
+                            <div className="tab-content">
+                                <div role="tabpanel" className="tab-pane active" id="home">
                                     <p>The full-length Max Air unit delivers excellent cushioning with enhanced flexibility for smoother transitions through footstrike.</p>
                                     <p>Dynamic Flywire cables integrate with the laces and wrap your midfoot for a truly adaptive, supportive fit.</p>
                                 </div>
-                                <div role="tabpanel" class="tab-pane" id="profile">
+                                <div role="tabpanel" className="tab-pane" id="profile">
                                     <p>Nike is one of the leading manufacturer and supplier of sports equipment, footwear and apparels. Nike is a global brand and it continuously creates products using high technology and design innovation. Nike has a vast collection of sports shoes for men at Snapdeal. You can explore our range of basketball shoes, football shoes, cricket shoes, tennis shoes, running shoes, daily shoes or lifestyle shoes. Take your pick from an array of sports shoes in vibrant colours like red, yellow, green, blue, brown, black, grey, olive, pink, beige and white. Designed for top performance, these shoes match the way you play or run. Available in materials like leather, canvas, suede leather, faux leather, mesh etc, these shoes are lightweight, comfortable, sturdy and extremely sporty. The sole of all Nike shoes is designed to provide an increased amount of comfort and the material is good enough to provide an improved fit. These shoes are easy to maintain and last for a really long time given to their durability. Buy Nike shoes for men online with us at some unbelievable discounts and great prices. So get faster and run farther with your Nike shoes and track how hard you can play.</p>
                                 </div>
-                                <div role="tabpanel" class="tab-pane" id="messages">
+                                <div role="tabpanel" className="tab-pane" id="messages">
                                     The images represent actual product though color of the image and product may slightly differ.
-                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="you-might-like">
-                    <div class="container">
-                        <h3 class="you-might">Products You May Like</h3>
-                        <div class="clearfix"></div>
+                <div className="you-might-like">
+                    <div className="container">
+                        <h3 className="you-might">Products You May Like</h3>
+                        <div className="clearfix"></div>
                     </div>
                 </div>
 
@@ -143,11 +127,11 @@ export class Single extends Component {
                     <MobileList />
                 </div>
 
-                <div class="footer-grid" style={{marginTop:"100px"}} >
-                    <div class="container">
-                        <div class="col-md-2 re-ft-grd">
+                <div className="footer-grid" style={{marginTop:"100px"}} >
+                    <div className="container">
+                        <div className="col-md-2 re-ft-grd">
                             <h3>Categories</h3>
-                            <ul class="categories">
+                            <ul className="categories">
                                 <li><a href="#">Men</a></li>
                                 <li><a href="#">Women</a></li>
                                 <li><a href="#">Kids</a></li>
@@ -156,9 +140,9 @@ export class Single extends Component {
                                 <li><a href="#">Sports</a></li>
                             </ul>
                         </div>
-                        <div class="col-md-2 re-ft-grd">
+                        <div className="col-md-2 re-ft-grd">
                             <h3>Short links</h3>
-                            <ul class="shot-links">
+                            <ul className="shot-links">
                                 <li><a href="#">Contact us</a></li>
                                 <li><a href="#">Support</a></li>
                                 <li><a href="#">Delivery</a></li>
@@ -167,20 +151,20 @@ export class Single extends Component {
                                 <li><a href="contact.html">Sitemap</a></li>
                             </ul>
                         </div>
-                        <div class="col-md-6 re-ft-grd">
+                        <div className="col-md-6 re-ft-grd">
                             <h3>Social</h3>
-                            <ul class="shot-links">
-                                <li><a href="#" class="fb">facebook</a></li>
-                                <li><a href="#" class="twt">twitter</a></li>
-                                <li><a href="#" class="gpls">g+ plus</a></li>
-                                <div class="clearfix"></div>
+                            <ul className="shot-links">
+                                <li><a href="#" className="fb">facebook</a></li>
+                                <li><a href="#" className="twt">twitter</a></li>
+                                <li><a href="#" className="gpls">g+ plus</a></li>
+                                <div className="clearfix"></div>
                             </ul>
                         </div>
                         
-                        <div class="clearfix"></div>
+                        <div className="clearfix"></div>
                     </div>
-                    <div class="copy-rt">
-                        <div class="container">
+                    <div className="copy-rt">
+                        <div className="container">
                             <p>&copy;   2015 N-AIR All Rights Reserved. Design by <a href="http://www.w3layouts.com">w3layouts</a></p>
                         </div>
                     </div>
