79bc4629d5c5fa9aed834bdd715df493d4366a34
superstark01 <superstark02@gmail.com>
2021-07-09 23:06:59 +0530

    123

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react';
 import firebase from 'firebase'
 import { updatePayment } from '../Database/uploadData'
 import getDoc from '../Database/getDoc';
-import sendMail from '../Database/sendMail';
+import emailjs from 'emailjs-com';
 
 
 class Success extends Component {
     state = {
         status: false,
-        e: null
+        e: null,
+        name: null,
+        product_name: null,
+        date: null,
+        amount: null,
+        to: null
+    }
+
+    sendMail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_060u1yu', e.target, 'user_rdnQ08wROAm4vj2HIcVdc')
+            .then((result) => {
+                this.setState({ status: true })
+            }, (error) => {
+                this.setState({
+                    status: <div style={{ color: "rgba(250,0,0,1)" }} >
+                        Payment was success: but there was some internal error: 501, report <a href="/contact" >here</a>
+                    </div>
+                })
+            });
     }
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+
                 getDoc("Products", "1pow").then(snap => {
                     updatePayment({
                         user_id: user.uid,
@@ -32,13 +53,23 @@ class Success extends Component {
                     })
                 })
             }
+            else {
+                alert("Please Sign In")
+            }
         });
     }
 
     render() {
         if (this.state.status) {
             return (
-                <div>
+                <div id="element" >
+                    <form id="mail" onSubmit={this.sendMail} >
+                        <input type="hidden" name="name" value={this.state.name} />
+                        <input type="hidden" name="name" value={this.state.product_name} />
+                        <input type="hidden" name="name" value={this.state.date} />
+                        <input type="hidden" name="name" value={this.state.amount} />
+                        <input type="hidden" name="name" value={this.state.to} />
+                    </form>
                     <div style={{ background: "transparent" }} >
                         <div class="modal-dialog modal-confirm">
                             <div class="modal-content">
@@ -71,8 +102,8 @@ class Success extends Component {
                 </div>
             );
         }
-        else{
-            return <div className="wrap" ><div>Please wait. Do not refresh or reload the page</div></div>
+        else {
+            return <div  id="element"  className="wrap" ><div>Please wait. Do not refresh or reload the page</div></div>
         }
     }
 }
